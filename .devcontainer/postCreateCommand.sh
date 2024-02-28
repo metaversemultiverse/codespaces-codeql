2f92b2803f7163bcbd4560e4e891c74fd2bf172b
Metaverse Multiverse <148008296+metaversemultiverse@users.noreply.github.com>
2024-02-28 12:00:32 -0800

    Initialize elena/open-workspace

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Copy the dbscheme into the tutorial library, so it matches the DB.
+cp .tours/codeql-tutorial-database/db-csv/csv.dbscheme tutorial-queries/
+cp .tours/codeql-tutorial-database/db-csv/csv.dbscheme.stats tutorial-queries/
